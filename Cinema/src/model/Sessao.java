11731e024e3100c06929c222075012fd0eee2d64
Joao Pedro <jguedespresto@gmail.com>
2017-11-23 09:08:28 -0200

    Classe Sessao

---
+++ Cinema/src/model/Sessao.java
@@ -0,0 +1,43 @@
+package model;
+
+/*
+ * @author JohnPeter
+ */
+public class Sessao {
+    private int id;
+    private double preco;
+    private int hora, min;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }   
+    
+}
