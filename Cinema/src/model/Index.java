408db904ac2f127736e00fc9d1a3476ef0c7d609
Joao Pedro <jguedespresto@gmail.com>
2017-11-23 09:21:04 -0200

    Classe Index

---
+++ Cinema/src/model/Index.java
@@ -0,0 +1,29 @@
+package model;
+
+/*
+ * @author JohnPeter
+ */
+public class Index {
+    private int id;
+    private boolean ocupada;
+
+    public Index() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }    
+    
+}
