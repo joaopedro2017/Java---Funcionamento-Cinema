fbaae1000cb2ef54cf1221427b159f8ca3e93189
Joao Pedro <jguedespresto@gmail.com>
2017-11-23 09:18:21 -0200

    Classe Poltrona

---
+++ Cinema/src/model/Poltrona.java
@@ -0,0 +1,25 @@
+package model;
+
+/*
+ * @author JohnPeter
+ */
+public class Poltrona {
+    private int fileira, coluna;
+
+    public int getFileira() {
+        return fileira;
+    }
+
+    public void setFileira(int fileira) {
+        this.fileira = fileira;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }    
+    
+}
