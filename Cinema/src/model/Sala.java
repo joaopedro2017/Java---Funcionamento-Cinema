e1b95785fdf77c18a1fd121fdce4ed08fafa78a9
Joao Pedro <jguedespresto@gmail.com>
2017-11-23 09:13:11 -0200

    Classe Sala

---
+++ Cinema/src/model/Sala.java
@@ -0,0 +1,42 @@
+package model;
+
+/*
+ * @author JohnPeter
+ */
+public class Sala {
+    private int numero, qntFileiras, qntColunas;
+    private String tipoSom;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getQntFileiras() {
+        return qntFileiras;
+    }
+
+    public void setQntFileiras(int qntFileiras) {
+        this.qntFileiras = qntFileiras;
+    }
+
+    public int getQntColunas() {
+        return qntColunas;
+    }
+
+    public void setQntColunas(int qntColunas) {
+        this.qntColunas = qntColunas;
+    }
+
+    public String getTipoSom() {
+        return tipoSom;
+    }
+
+    public void setTipoSom(String tipoSom) {
+        this.tipoSom = tipoSom;
+    }   
+    
+}
